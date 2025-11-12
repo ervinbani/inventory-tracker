c61257b90eefe08fc1322ef1b6e69489befd1d39
ervinbani <bani.ervin@gmail.com>
2025-11-12 21:03:45 +0100

    Add physucalproducts class

---
+++ src/models/PhysicalProducts.ts
@@ -0,0 +1,24 @@
+import { Product } from "./Products.js";
+
+export class PhysicalProduct extends Product {
+  weight: number;
+
+  constructor(sku: string, name: string, price: number, weight: number) {
+    super(sku, name, price);
+    this.weight = weight;
+  }
+
+  /**
+   * Getter method to return the formatted weight in kilograms
+   */
+  get formattedWeight(): string {
+    return `${this.weight} kg`;
+  }
+
+  /**
+   * Override getPriceWithTax to calculate final price with 10% tax rate
+   */
+  getPriceWithTax(): number {
+    return this.price * 1.1;
+  }
+}
